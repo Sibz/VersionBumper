d4d45c09c8fdfa40d6133bafeecb3903d784075d
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-25 13:34:19 +1000

    Added colons to test.ts

--- tests/test.ts
+++ tests/test.ts
@@ -3,8 +3,8 @@ import * as vb from '../src/ts/VersionBumper';
 
 test('checkAccessToFile: when packageFile does not exist should return false', async t=>{
     t.false(await vb.checkAccessToFile("invalid file"));
-})
+});
 
 test('checkAccessToFile: when packageFile does exist should return true', async t=>{
     t.true(await vb.checkAccessToFile("./package/package.json"));
-})
+});
