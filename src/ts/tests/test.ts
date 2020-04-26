bdb5a37cb617ef77b5ccffbfc9ee4e5043e5d4ff
Steven Turner <12050358+Sibz@users.noreply.github.com>
2020-04-26 14:12:34 +1000

    Added tests to check actual incrementaion When using constructor args.

--- src/ts/tests/test.ts
+++ src/ts/tests/test.ts
@@ -3,6 +3,7 @@ import VersionBumper, * as vb from '../VersionBumper';
 import { SemVerParts, Version, parseSemVer, semVerToString } from '../parseSemVer';
 import { promises as fs } from "fs";
 const TEST_FILE_PATH: string = "./testPackage.json";
+const STATIC_TEST_FILE_PATH: string = "./package/package.json";
 
 test('checkAccessToFile: when packageFile does not exist should return false', async t => {
     t.false(await vb.checkAccessToFile("invalid file"));
@@ -65,10 +66,9 @@ test.serial('updateVersion: When dontWrite set, Should not update file', async t
     let ver: Version = { M: 1, m: 2, p: 3, build: "alpha", meta: "meta" };
     let testFilePath = TEST_FILE_PATH;
     await fs.writeFile(testFilePath, JSON.stringify( {version: semVerToString(ver)}, null, 2));
-    await new VersionBumper(testFilePath).updateVersion();
+    await new VersionBumper(testFilePath, undefined, true).updateVersion();
     let verActual = parseSemVer(vb.getVersionFromPackage(await vb.getJSONObjectFromFile(testFilePath)));
-    let verExpected = vb.bumpVersion(ver,SemVerParts.Patch);
-    t.deepEqual(verActual, verExpected);
+    t.deepEqual(verActual, ver);
 });
 
 test.afterEach.always('updateVersion: When dontWrite set, Should not update file', async ()=> {
@@ -77,4 +77,22 @@ test.afterEach.always('updateVersion: When dontWrite set, Should not update file
     } catch {
 
     }
+});
+
+test('updateVersion: When defaults, should increment patch', async t=> {
+    let expected: Version = {M:1,m:0,p:1,build:undefined,meta:undefined};
+    let actual = await new VersionBumper(STATIC_TEST_FILE_PATH, undefined, true).updateVersion();
+    t.deepEqual(actual, expected);
+});
+
+test('updateVersion: When minor, should increment minor', async t=> {
+    let expected: Version = {M:1,m:1,p:0,build:undefined,meta:undefined};
+    let actual = await new VersionBumper(STATIC_TEST_FILE_PATH, SemVerParts.Minor, true).updateVersion();
+    t.deepEqual(actual, expected);
+});
+
+test('updateVersion: When major, should increment major', async t=> {
+    let expected: Version = {M:2,m:0,p:0,build:undefined,meta:undefined};
+    let actual = await new VersionBumper(STATIC_TEST_FILE_PATH, SemVerParts.Major, true).updateVersion();
+    t.deepEqual(actual, expected);
 });
